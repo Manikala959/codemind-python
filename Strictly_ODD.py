017263b5c8d5c6ec42fb7a209906ecf55f474fdb
Manikala959 <96293890+Manikala959@users.noreply.github.com>
2023-07-08 15:18:33 +0530

    n=int(input()) l=list(map(int,input().split())) f=0 for i in range(len(l)):     if l[i]%2==0:         if i%2!=0:             f=1
    if(f==1):
        print("False")
    else:
        print("True")

---
+++ Strictly_ODD.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=list(map(int,input().split()))
+f=0
+for i in range(len(l)):
+    if l[i]%2==0:
+        if i%2!=0:
+            f=1
+        
+if(f==1):
+    print("False")
+else:
+    print("True")
